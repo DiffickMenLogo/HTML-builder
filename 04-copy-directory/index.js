d544ed9b2b073c5edb2775b6ba4c40b297d5ccad
DiffickMenLogo <gh.sdfs@mail.ru>
2022-05-15 17:32:38 +0300

    feat: made 04-copy-directory(15 May, 17:32)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const folder = '04-copy-directory/files';
 
+fs.readdir(`04-copy-directory/files-copy`, (err) =>{
+    if(err) {
+        fs.mkdir(`04-copy-directory/files-copy`, (err) => {
+            if(err) {
+                console.log('Error', err.message);
+                return;
+            }
+            console.log('Folder created');
+        });
+    };
+});
+
+fs.readdir(folder, (err, files) => {
+    if(err) {
+        console.log('Error', err.message);
+        return;
+    }else{
+        files.forEach(file => {
+            fs.copyFile(`${folder}/${file}`, `04-copy-directory/files-copy/${file}`, (err) => {
+                if(err) {
+                    console.log('Error', err.message);
+                    return;
+                };
+                console.log('File copied');
+            });
+        });
+    }
+});
